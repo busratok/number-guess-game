259260ffae2962883fa98d5d172e3aca0503a2db
busratok <busratokuc@gmail.com>
2023-01-17 15:11:44 +0000

    Event listener is optimised

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
-const btn = document.getElementById("btn");
-const randomNum = Math.floor(Math.random() * 100) + 1;
+const buttons = document.querySelector(".buttons");
 const attempts = document.getElementById("attempts");
 const result = document.getElementById("result");
 const input = document.getElementById("number");
-const reset = document.getElementById("reset");
-console.log(randomNum);
+const randomNum = Math.floor(Math.random() * 100) + 1;
 
 let counter = 1;
 let lowest = 1;
 let highest = 100;
-btn.addEventListener("click", () => {
-  let number = document.getElementById("number").value;
-  input.value = "";
-  attempts.textContent = `${counter}`;
-  if (counter > 9) {
-    result.textContent = "You lose!";
-  } else if (number == randomNum) {
-    result.textContent = `Congrats`;
-    input.disabled = true;
-  } else {
-    if (number < randomNum) {
-      lowest = number;
-    } else if (number > randomNum) {
-      highest = number;
+buttons.addEventListener("click", (e) => {
+  if (e.target.id === "btn") {
+    let number = document.getElementById("number").value;
+    if (number > 0 && number < 101) {
+      input.value = "";
+      attempts.textContent = `${counter}`;
+      if (counter > 9) {
+        result.style.color = "#AA1945";
+        result.style.fontSize = "2rem";
+        result.textContent = "You lose!";
+
+        input.disabled = true;
+      } else if (number == randomNum) {
+        result.style.color = "#F9CCD3";
+        result.style.fontSize = "2rem";
+        result.textContent = `Congrats Number is ${number}`;
+        input.disabled = true;
+      } else {
+        if (number < randomNum) {
+          lowest = number;
+        } else if (number > randomNum) {
+          highest = number;
+        }
+        result.textContent = `Enter a number between ${lowest} to ${highest}`;
+        counter++;
+      }
+    } else {
+      result.textContent = `Enter a number between ${lowest} to ${highest}`;
     }
-    result.textContent = `Enter a number between ${lowest} to ${highest}`;
-    counter++;
+  } else if (e.target.id === "reset") {
+    window.location.reload();
   }
-});
-reset.addEventListener("click", () => {
-  input.disabled = false;
-  counter = 1;
-  attempts.textContent = "";
-  result.textContent = "";
-  lowest = 1;
-  highest = 100;
 });
